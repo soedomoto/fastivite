8e7d25c6ed05d3d2b19b8e1fff6a57bd5d9548c1
soedomoto <soedomoto@gmail.com>
2024-06-16 17:01:05 +0700

    graphql 1.0.18

--- packages/graphql/src/cli.ts
+++ packages/graphql/src/cli.ts
@@ -1,5 +1,3 @@
-#! /usr/bin/env node
-
 import { program } from 'commander';
 import { buildGraphqlServer } from './build.js';
 import { createGraphqlDevServer } from './dev.js';
@@ -10,7 +8,7 @@ import { previewGraphqlServer } from './preview.js';
     .command('dev')
     .description('dev')
     .option('--host <host>', 'Host', '127.0.0.1')
-    .option('--port <port>', 'Port', '6754')
+    .option('--port <port>', 'Port', parseInt, 8081)
     .option('--base <base>', 'Base', '')
     .option('--index <index>', 'index.html', './index.html')
     .option(
@@ -36,27 +34,86 @@ import { previewGraphqlServer } from './preview.js';
     .option(
       '--graphql-schema-cwd <graphql-schema-cwd>',
       'graphql schema working directory. e.g: ./graphql/schema',
-      './graphql/schema'
+      './src/graphql/schema'
     )
     .option(
       '--graphql-schema-pattern <graphql-schema-pattern...>',
       'graphql schema pattern. e.g: **/*.gql',
-      ['**/*.gql', '**/*.graphql', '**/*.graphqls']
+      ['**/*.gql', '**/*.graphql']
+    )
+    .option(
+      '--graphql-resolver-cwd <graphql-resolver-cwd>',
+      'graphql resolver working directory. e.g: ./graphql/resolver',
+      './src'
+    )
+    .option(
+      '--graphql-resolver-pattern <graphql-resolver-pattern...>',
+      'graphql resolver pattern. e.g: **/*.resolver.ts',
+      ['**/*.resolver.ts', '**/*.resolver.js']
+    )
+    .option(
+      '--graphql-loader-cwd <graphql-loader-cwd>',
+      'graphql loader working directory. e.g: ./graphql/loader',
+      './src'
+    )
+    .option(
+      '--graphql-loader-pattern <graphql-loader-pattern...>',
+      'graphql loader pattern. e.g: **/*.loader.js',
+      ['**/*.loader.ts', '**/*.loader.js']
+    )
+    .option(
+      '--graphql-context-cwd <graphql-context-cwd>',
+      'graphql context working directory. e.g: ./graphql/context',
+      './src'
+    )
+    .option(
+      '--graphql-context-pattern <graphql-context-pattern...>',
+      'graphql context pattern. e.g: **/*.context.js',
+      ['**/*.context.ts', '**/*.context.js']
+    )
+    .option(
+      '--graphql-codegen <graphql-codegen>',
+      'is codegen enabled',
+      true
     )
     .option(
       '--graphql-codegen-out <graphql-codegen-out...>',
       'graphql-codegen-out. e.g: gen.ts',
-      './graphql/schema-gen.ts'
+      './src/graphql/gen.ts'
     )
-    .option('--build-dir <build-dir>', 'Build directory', 'build')
+    .option(
+      '--graphql-operation-codegen <graphql-operation-codegen>',
+      'graphql-operation-codegen',
+      true
+    )
+    .option(
+      '--graphql-operation-codegen-config-file <graphql-operation-codegen-config-file>',
+      'graphql-operation-codegen-config-file',
+      './codegen.js'
+    )
+    
     .action((params) => {
       createGraphqlDevServer(params);
     });
+
+// graphqlSchemaCwd: string;
+// graphqlSchemaPattern: string | string[];
+// graphqlResolverCwd: string;
+// graphqlResolverPattern: string | string[];
+// graphqlLoaderCwd: string;
+// graphqlLoaderPattern: string | string[];
+// graphqlContextCwd: string;
+// graphqlContextPattern: string | string[];
 
   program
     .command('build')
     .description('build')
     .option('--out-dir <out-dir>', 'Output directory', 'dist')
+    .option(
+      '--config-file <config-file>',
+      'vite.config.(j|t)sx',
+      './vite.config.ts'
+    )
     .option(
       '--entry-server <entry-server>',
       'entry-server.(j|t)sx',
@@ -75,17 +132,42 @@ import { previewGraphqlServer } from './preview.js';
     .option(
       '--graphql-schema-cwd <graphql-schema-cwd>',
       'graphql schema working directory. e.g: ./graphql/schema',
-      './graphql/schema'
+      './src/graphql/schema'
     )
     .option(
       '--graphql-schema-pattern <graphql-schema-pattern...>',
       'graphql schema pattern. e.g: **/*.gql',
-      ['**/*.gql', '**/*.graphql', '**/*.graphqls']
+      ['**/*.gql', '**/*.graphql']
     )
     .option(
-      '--graphql-codegen-out <graphql-codegen-out...>',
-      'graphql-codegen-out. e.g: gen.ts',
-      './graphql/schema-gen.ts'
+      '--graphql-resolver-cwd <graphql-resolver-cwd>',
+      'graphql resolver working directory. e.g: ./graphql/resolver',
+      './src'
+    )
+    .option(
+      '--graphql-resolver-pattern <graphql-resolver-pattern...>',
+      'graphql resolver pattern. e.g: **/*.resolver.ts',
+      ['**/*.resolver.ts', '**/*.resolver.js']
+    )
+    .option(
+      '--graphql-loader-cwd <graphql-loader-cwd>',
+      'graphql loader working directory. e.g: ./graphql/loader',
+      './src'
+    )
+    .option(
+      '--graphql-loader-pattern <graphql-loader-pattern...>',
+      'graphql loader pattern. e.g: **/*.loader.js',
+      ['**/*.loader.ts', '**/*.loader.js']
+    )
+    .option(
+      '--graphql-context-cwd <graphql-context-cwd>',
+      'graphql context working directory. e.g: ./graphql/context',
+      './src'
+    )
+    .option(
+      '--graphql-context-pattern <graphql-context-pattern...>',
+      'graphql context pattern. e.g: **/*.context.js',
+      ['**/*.context.ts', '**/*.context.js']
     )
     .action((params) => {
       buildGraphqlServer(params);
